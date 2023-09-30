04be78941f4c1919f7b8ae52c40fb39efb168a31
Nikita Rukin <n.rukin@gmail.com>
2023-09-30 20:34:03 +0300

    Обработка пост-запроса

--- src/code.js
+++ src/code.js
@@ -2,5 +2,9 @@ function doGet() {
     var t = HtmlService.createTemplateFromFile('page');
     t.data = MockTest();
     return t.evaluate();
+}
 
+function processForm(formObject) {
+  Logger.log(formObject);
+    return "10% верных ответов"
 }
