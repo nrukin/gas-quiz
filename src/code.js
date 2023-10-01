077f4990e4ec0c34b1a8b00b215b9a85a37a4a62
Nikita Rukin <n.rukin@gmail.com>
2023-10-01 10:33:28 +0300

    Проверка результата теста

--- src/code.js
+++ src/code.js
@@ -6,5 +6,13 @@ function doGet() {
 
 function processForm(formObject) {
     Logger.log(formObject);
-    return "10% верных ответов"
+    var data = MockTest();
+    var rs = 0;
+    for (var i = 0; i < data.length; i++) {
+	if (data[i].rightAnsID == formObject[data[i].id]) {
+	    rs++
+	}
+    }
+    Logger.log(rs)
+    return "Правильных ответов: " + rs + " / " + data.length;
 }
