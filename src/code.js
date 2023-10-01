b81226c3793d6766b63133487b787b2f320ceb6b
Nikita Rukin <n.rukin@gmail.com>
2023-10-01 13:38:04 +0300

    Получаю данные теста со страницы таблицы

--- src/code.js
+++ src/code.js
@@ -1,12 +1,12 @@
 function doGet() {
     var t = HtmlService.createTemplateFromFile('page');
-    t.data = MockTest();
+    t.data = getTestData();
     return t.evaluate();
 }
 
 function processForm(formObject) {
     Logger.log(formObject);
-    var data = MockTest();
+    var data = getTestData();
     var rs = 0;
     for (var i = 0; i < data.length; i++) {
 	if (data[i].rightAnsID == formObject[data[i].id]) {
@@ -23,4 +23,24 @@ function addResultRow(name, tot, rs) {
 	[new Date(),name,rs,tot]
     );
 
+}
+
+function getTestData() {
+    var rs = [];
+    var val = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Вопросы").getDataRange().getValues();
+
+    for (var i= 1; i < val.length; i++) {
+	if (!val[i][1]) {
+	    break
+	}
+	q = new question("q" + i, val[i][1]);
+	for (var j = 3; j < val[i].length; j = j + 2) {
+	    if (!val[i][j]) {
+		break
+	    }
+	    q.addAnswer(val[i][j], (val[i][j-1] == true))
+	}
+	rs.push(q)
+    }
+    return rs
 }
